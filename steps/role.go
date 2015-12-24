d5344971c8566b6e7ad5ad12b870903bb0876f3c
Vincent Behar <v.behar@free.fr>
2015-12-24 09:10:31 +0100

    add more role steps

--- steps/role.go
+++ steps/role.go
@@ -11,29 +11,41 @@ import (
 func init() {
 	RegisterSteps(func(c *Context) {
 
+		c.Then(`The user "(.+?)" should have the "(.+?)" role$`, func(userName string, roleName string) {
+			c.checkUserHasRole(userName, roleName)
+		})
+
 		c.Then(`I should have the "(.+?)" role$`, func(roleName string) {
-			rb, err := c.GetRoleBindingForRole(roleName)
-			if err != nil {
-				c.Fail("Failed to get Role Binding for role '%s': %v", roleName, err)
-				return
-			}
-			if rb == nil {
-				c.Fail("Could not find a Role Binding for role '%s'", roleName)
-				return
-			}
-
-			user, err := c.GetCurrentUser()
-			if err != nil {
-				c.Fail("Failed to get the current User: %v", err)
-				return
-			}
-
-			if !rb.Users.Has(user.Name) {
-				c.Fail("The current user '%s' does not have the '%s' role !", user.Name, roleName)
-			}
+			c.checkUserHasRole("", roleName)
 		})
 
 	})
+}
+
+// checkUserHasRole checks that the given user has the given role
+func (c *Context) checkUserHasRole(userName string, roleName string) {
+	rb, err := c.GetRoleBindingForRole(roleName)
+	if err != nil {
+		c.Fail("Failed to get Role Binding for role '%s': %v", roleName, err)
+		return
+	}
+	if rb == nil {
+		c.Fail("Could not find a Role Binding for role '%s'", roleName)
+		return
+	}
+
+	if len(userName) == 0 {
+		user, err := c.GetCurrentUser()
+		if err != nil {
+			c.Fail("Failed to get the current User: %v", err)
+			return
+		}
+		userName = user.Name
+	}
+
+	if !rb.Users.Has(userName) {
+		c.Fail("The user '%s' does not have the '%s' role !", userName, roleName)
+	}
 }
 
 // GetRoleBinding gets the RoleBinding with the given role name, or returns an error
